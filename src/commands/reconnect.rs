50742094388251291da0263d0bd1f3f1f96dd34e
WardLordRuby <chad2lax@gmail.com>
2025-03-15 23:53:28 -0500

    Cleanup: readability changes

--- src/commands/reconnect.rs
+++ src/commands/reconnect.rs
@@ -10,6 +10,7 @@ use crate::{
 
 use std::{
     borrow::Cow, collections::HashMap, fmt::Display, net::SocketAddr, sync::atomic::Ordering,
+    time::Duration,
 };
 
 use repl_oxide::ansi_code::{RESET, YELLOW};
@@ -18,6 +19,7 @@ use tracing::{error, info};
 use winptyrs::PTY;
 
 pub const HISTORY_MAX: usize = 6;
+const CONSEC_CMD_DELAY: Duration = Duration::from_millis(15);
 
 struct DisplayHistory<'a>(&'a [HostName], &'a [Cow<'static, str>]);
 
@@ -75,11 +77,13 @@ impl CommandContext {
             info!("No joined servers in history, connect to a server to add it to history");
             return Ok(CommandHandle::Processed);
         }
+
         if args.history {
             display_history(&cache.connection_history, &cache.host_to_connect);
             return Ok(CommandHandle::Processed);
         }
-        let lock = match self.check_h2m_connection().await {
+
+        let rw_lock = match self.check_h2m_connection().await {
             Ok(lock) => lock,
             Err(err) => {
                 error!("{err}");
@@ -88,20 +92,20 @@ impl CommandContext {
             }
         };
 
-        let mut target = cache.connection_history.len() - 1;
-        let mut modify_cache = false;
-        if let Some(num) = args.connect.map(|n| n as usize) {
-            if num > 1 {
+        let (target_i, modify_cache) =
+            if let Some(num) = args.connect.and_then(|i| (i > 1).then(|| usize::from(i))) {
                 if num > cache.connection_history.len() {
                     error!("{}", DisplayHistoryErr(cache.connection_history.len()));
                     return Ok(CommandHandle::Processed);
                 }
-                (target, modify_cache) = (cache.connection_history.len() - num, true);
-            }
-        }
+                (cache.connection_history.len() - num, true)
+            } else {
+                (cache.connection_history.len() - 1, false)
+            };
+
         let Some(&ip_port) = cache
             .host_to_connect
-            .get(&cache.connection_history[target].raw)
+            .get(&cache.connection_history[target_i].raw)
         else {
             error!("Could not find server in cache");
             println!(
@@ -110,7 +114,7 @@ impl CommandContext {
             return Ok(CommandHandle::Processed);
         };
 
-        if let Err(err) = connect_to(ip_port, &lock).await {
+        if let Err(err) = connect_to(ip_port, &rw_lock).await {
             error!("{err}");
             return Ok(CommandHandle::Processed);
         }
@@ -118,7 +122,7 @@ impl CommandContext {
         info!(name: LOG_ONLY, "Connected to {ip_port}");
 
         if modify_cache {
-            let entry = cache.connection_history.remove(target);
+            let entry = cache.connection_history.remove(target_i);
             cache.connection_history.push(entry);
             self.cache_needs_update().store(true, Ordering::SeqCst);
         }
@@ -127,9 +131,9 @@ impl CommandContext {
 }
 
 /// Before calling be sure to guard against invalid handles by checking `.check_h2m_connection().is_ok()`
-async fn connect_to(ip_port: SocketAddr, lock: &RwLock<PTY>) -> Result<(), Cow<'_, str>> {
-    let game_console = lock.read().await;
+async fn connect_to(ip_port: SocketAddr, rw_lock: &RwLock<PTY>) -> Result<(), Cow<'_, str>> {
+    let game_console = rw_lock.read().await;
     game_console.send_cmd("disconnect")?;
-    tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
+    tokio::time::sleep(CONSEC_CMD_DELAY).await;
     game_console.send_cmd(format!("connect {ip_port}"))
 }
