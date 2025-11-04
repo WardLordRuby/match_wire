WardLordRuby/match_wire

commit 62c7456873bb677702e65167e495404f68e1d68f

Feat: Modify defaults
Users can now use the `settings` command to open an interactive settings
window. Here they are able to modify MatchWire settings and default
arguments for `filter` queries.

Settings are stored as a json file in %localappdata%/match_wire

src/lib.rs
@@ -1,8 +1,9 @@
 pub mod commands {
     pub mod filter;
     pub mod handler;
-    pub mod launch_h2m;
+    pub mod launch;
     pub mod reconnect;
+    pub mod settings;
 }
 pub mod models {
     pub mod cli;
@@ -19,16 +20,16 @@ pub mod utils {
 use crate::{
     commands::{
         handler::{Message, ReplHandle, StartupCacheContents},
-        launch_h2m::get_exe_version,
+        launch::get_exe_version,
     },
     models::{
         cli::Command,
         json_data::{CacheFile, HmwManifest, StartupInfo},
     },
     utils::{
-        caching::{ReadCacheErr, build_cache, read_cache},
+        caching::{ReadCacheErr, build_cache},
         display::{self, table::TABLE_PADDING},
-        global_state,
+        global_state::{self, AltScreen},
         subscriber::init_subscriber,
     },
 };
@@ -64,8 +65,8 @@ pub const LOG_ONLY: &str = "log_only";
 
 const MOD_FILES_MODULE_NAME: &str = "mod";
 
-pub const H2M_MAX_CLIENT_NUM: i64 = 18;
-pub(crate) const H2M_MAX_TEAM_SIZE: i64 = 9;
+pub const H2M_MAX_CLIENT_NUM: u8 = 18;
+pub(crate) const H2M_MAX_TEAM_SIZE: u8 = 9;
 
 pub const SAVED_HISTORY_CAP: usize = 20;
 
@@ -100,7 +101,6 @@ pub const LOCAL_DATA: &str = "LOCALAPPDATA";
 pub(crate) const CACHED_DATA: &str = "cache.json";
 
 pub mod splash_screen {
-    use crate::commands::handler::Message;
     use std::io;
 
     #[cfg(not(debug_assertions))]
@@ -110,57 +110,7 @@ pub mod splash_screen {
     };
 
     #[cfg(not(debug_assertions))]
-    pub(crate) use release::*;
-
-    #[cfg(not(debug_assertions))]
-    mod release {
-
-        use super::*;
-
-        use std::cell::{Cell, RefCell};
-
-        use tracing_subscriber::fmt::format::Writer;
-
-        thread_local! {
-            pub(super) static SPLASH_SCREEN_VIS: Cell<bool> = const { Cell::new(false) };
-            pub(super) static SPLASH_SCREEN_EVENT_BUFFER:
-                RefCell<SplashScreenEvents> = RefCell::new(SplashScreenEvents::default());
-        }
-
-        #[derive(Default)]
-        pub(super) struct SplashScreenEvents {
-            pub(super) pre_subscriber: Vec<Message>,
-            pub(super) from_subscriber: String,
-        }
-
-        pub(crate) fn is_visible() -> bool {
-            SPLASH_SCREEN_VIS.get()
-        }
-
-        pub(crate) fn push_formatter<F>(print: F) -> std::fmt::Result
-        where
-            F: FnOnce(Writer<'_>) -> std::fmt::Result,
-        {
-            SPLASH_SCREEN_EVENT_BUFFER
-                .with_borrow_mut(|buf| print(Writer::new(&mut buf.from_subscriber)))
-        }
-    }
-
-    /// **Only** use for errors encountered before tracing subscriber has been initialized otherwise prefer a tracing
-    /// event as our subscriber format layer takes care of fn call behind the scenes
-    pub fn push_message(message: Message) {
-        #[cfg(debug_assertions)]
-        message.record();
-
-        #[cfg(not(debug_assertions))]
-        {
-            if !SPLASH_SCREEN_VIS.get() {
-                return message.record();
-            }
-
-            SPLASH_SCREEN_EVENT_BUFFER.with_borrow_mut(|buf| buf.pre_subscriber.push(message))
-        }
-    }
+    use crate::global_state::AltScreen;
 
     pub async fn enter() -> io::Result<()> {
         #[cfg(not(debug_assertions))]
@@ -175,7 +125,7 @@ pub mod splash_screen {
 
             let mut stdout = std::io::stdout();
 
-            SPLASH_SCREEN_VIS.set(true);
+            AltScreen::enter();
             execute!(stdout, terminal::EnterAlternateScreen)?;
 
             let (columns, rows) = terminal::size()?;
@@ -208,16 +158,7 @@ pub mod splash_screen {
         #[cfg(not(debug_assertions))]
         {
             execute!(std::io::stdout(), terminal::LeaveAlternateScreen).unwrap();
-            SPLASH_SCREEN_VIS.set(false);
-
-            let events = SPLASH_SCREEN_EVENT_BUFFER.take();
-
-            events
-                .pre_subscriber
-                .into_iter()
-                .for_each(|message| message.record());
-
-            print!("{}", events.from_subscriber)
+            AltScreen::leave();
         }
     }
 }
@@ -253,28 +194,32 @@ impl From<pgp::errors::Error> for ResponseErr {
     }
 }
 
-pub type LoggerRes = (Option<PathBuf>, Option<Result<CacheFile, ReadCacheErr>>);
+/// Returns the working `%localappdata%` for this program
+pub fn try_init_logger() -> Option<PathBuf> {
+    let local_dir = std::env::var_os(LOCAL_DATA);
+    let local_dir = local_dir.as_ref().map(Path::new);
 
-pub fn try_init_logger() -> LoggerRes {
-    let mut local_dir = std::env::var_os(LOCAL_DATA).map(PathBuf::from);
-    let mut cache_res = None;
-    if let Some(ref mut dir) = local_dir {
-        if let Err(err) = check_app_dir_exists(dir) {
-            splash_screen::push_message(Message::error(err.to_string()));
-        } else {
-            init_subscriber(dir)
-                .unwrap_or_else(|err| splash_screen::push_message(Message::error(err.to_string())));
-            info!(name: LOG_ONLY, "App startup");
-            cache_res = Some(read_cache(dir));
+    if let Some(local) = local_dir {
+        match check_app_dir_exists(local) {
+            Ok(app_dir_local) => {
+                init_subscriber(&app_dir_local)
+                    .unwrap_or_else(|err| AltScreen::push_message(Message::error(err.to_string())));
+                info!(name: LOG_ONLY, "App startup");
+                Some(app_dir_local)
+            }
+            Err(err) => {
+                AltScreen::push_message(Message::error(err.to_string()));
+                None
+            }
         }
     } else {
-        splash_screen::push_message(Message::error("Could not find %appdata%/local"));
+        AltScreen::push_message(Message::error("Could not find %appdata%/local"));
 
         #[cfg(debug_assertions)]
         init_subscriber(std::path::Path::new("")).unwrap();
-    }
 
-    (local_dir, cache_res)
+        None
+    }
 }
 
 pub(crate) fn client_with_timeout(secs: u64) -> Client {
@@ -435,20 +380,30 @@ pub fn await_user_for_end<D: Display>(err: D) {
     let _ = reader.read_line(&mut String::new());
 }
 
-/// Validates local/app_dir exists and modifies input if valid
-fn check_app_dir_exists(local: &mut PathBuf) -> io::Result<()> {
+/// Validates `%localappdata%` structure and returns an owned path to this programs own directory
+fn check_app_dir_exists(local: &Path) -> io::Result<PathBuf> {
     const PREV_NAME: &str = "h2m_favorites";
     let prev_local_dir = local.join(PREV_NAME);
+    let curr_local_dir = local.join(CRATE_NAME);
 
-    local.push(CRATE_NAME);
-    if !local.try_exists()? {
+    if !curr_local_dir.try_exists()? {
         std::fs::create_dir(local)?;
     }
 
-    if prev_local_dir.try_exists()? {
+    if prev_local_dir
+        .try_exists()
+        .map_err(|err| {
+            AltScreen::push_message(Message::error(format!(
+                "{err}, looking for {}",
+                prev_local_dir.display()
+            )))
+        })
+        .unwrap_or_default()
+    {
         std::fs::remove_dir_all(prev_local_dir)?;
     }
-    Ok(())
+
+    Ok(curr_local_dir)
 }
 
 pub(crate) fn make_slice_ascii_lowercase(vec: &mut [String]) {
@@ -518,10 +473,14 @@ pub fn print_help() {
 }
 
 pub(crate) async fn send_msg_over(sender: &tokio::sync::mpsc::Sender<Message>, message: Message) {
-    sender
-        .send(message)
-        .await
-        .unwrap_or_else(|returned| returned.0.log());
+    if AltScreen::is_visible() {
+        AltScreen::push_message(message);
+    } else {
+        sender
+            .send(message)
+            .await
+            .unwrap_or_else(|returned| returned.0.log());
+    }
 }
 
 pub async fn startup_cache_task(

src/commands/filter/mod.rs
@@ -6,7 +6,10 @@ pub use strategies::{FilterPreProcess, GameStats, process_stats};
 
 use crate::{
     LOG_ONLY, RateLimiter, ResponseErr, STATUS_OK, command_err,
-    commands::handler::{CmdErr, ReplHandle},
+    commands::{
+        handler::{CmdErr, CommandContext, ReplHandle},
+        settings::Settings,
+    },
     impl_rate_limit_config,
     models::{
         cli::{Filters, Region, Source},
@@ -25,7 +28,6 @@ use std::{
     fs::File,
     io::{self, Write},
     net::{AddrParseError, IpAddr, SocketAddr, ToSocketAddrs},
-    path::Path,
     time::Duration,
 };
 
@@ -41,7 +43,6 @@ const FAVORITES: &str = "favourites.json";
 
 pub(crate) const DEFAULT_SOURCES: [Source; 2] = [Source::Iw4Master, Source::HmwMaster];
 const DEFAULT_H2M_SERVER_CAP: usize = 100;
-const DEFAULT_INFO_RETRIES: u8 = 3;
 
 const RETRY_TIME_SCALE: Duration = Duration::from_millis(800);
 
@@ -51,10 +52,15 @@ pub(crate) const GAME_ID: [&str; 2] = ["H2M", "HMW"];
 pub(crate) const H2M_ID: &str = GAME_ID[0];
 pub(crate) const HMW_ID: &str = GAME_ID[1];
 
-const NA_CONT_CODE: [ContCode; 1] = [[b'N', b'A']];
-const SA_CONT_CODE: [ContCode; 1] = [[b'S', b'A']];
-const EU_CONT_CODES: [ContCode; 2] = [[b'E', b'U'], [b'A', b'F']];
-const APAC_CONT_CODES: [ContCode; 2] = [[b'A', b'S'], [b'O', b'C']];
+pub(crate) mod continent {
+    use super::ContCode;
+
+    pub(crate) const COUNT: usize = 4;
+    pub(crate) const NA_CODE: [ContCode; 1] = [[b'N', b'A']];
+    pub(crate) const SA_CODE: [ContCode; 1] = [[b'S', b'A']];
+    pub(crate) const EU_CODES: [ContCode; 2] = [[b'E', b'U'], [b'A', b'F']];
+    pub(crate) const APAC_CODES: [ContCode; 2] = [[b'A', b'S'], [b'O', b'C']];
+}
 
 fn serialize_json(f: &mut std::fs::File, from: String) -> io::Result<()> {
     const COMMA: char = ',';
@@ -68,15 +74,16 @@ fn serialize_json(f: &mut std::fs::File, from: String) -> io::Result<()> {
 
 #[instrument(name = "filter", level = "trace", skip_all)]
 pub(crate) async fn build_favorites(
+    ctx: &CommandContext,
     repl: &mut ReplHandle,
-    curr_dir: &Path,
-    args: Filters,
-    version: f64,
+    args: Option<Filters>,
 ) -> Result<bool, CmdErr> {
     let mut ip_collected = 0;
     let mut ips = String::new();
+    let args = args.unwrap_or_default();
+    let version = ctx.game_version().unwrap_or(1.0);
 
-    let mut favorites_path = curr_dir.join(FAVORITES_LOC);
+    let mut favorites_path = ctx.get_exe_dir().join(FAVORITES_LOC);
     match favorites_path.try_exists() {
         Ok(true) => (),
         Ok(false) => {
@@ -114,7 +121,7 @@ pub(crate) async fn build_favorites(
         )
     }
 
-    let mut filter = filter_server_list(repl, args, spinner).await?;
+    let mut filter = filter_server_list(repl, args, &ctx.settings, spinner).await?;
 
     if filter.duplicates != 0 {
         println!(
@@ -159,23 +166,46 @@ pub(crate) struct FilterData {
     cache_modified: bool,
 }
 
-impl Region {
-    fn to_bytes(self) -> &'static [ContCode] {
+pub(crate) enum RegionContainer {
+    Set(HashSet<ContCode>),
+    Vec(Vec<ContCode>),
+}
+
+impl RegionContainer {
+    pub(crate) fn contains(&self, region: &ContCode) -> bool {
         match self {
-            Region::Apac => &APAC_CONT_CODES,
-            Region::EU => &EU_CONT_CODES,
-            Region::NA => &NA_CONT_CODE,
-            Region::SA => &SA_CONT_CODE,
+            RegionContainer::Set(hash_set) => hash_set.contains(region),
+            RegionContainer::Vec(items) => items.contains(region),
         }
     }
 
-    fn to_byte_set(regions: &[Region]) -> HashSet<ContCode> {
-        regions
-            .iter()
-            .copied()
-            .flat_map(Region::to_bytes)
-            .copied()
-            .collect()
+    pub(crate) fn len(&self) -> usize {
+        match self {
+            RegionContainer::Set(hash_set) => hash_set.len(),
+            RegionContainer::Vec(items) => items.len(),
+        }
+    }
+}
+
+impl Region {
+    fn to_bytes(self) -> &'static [ContCode] {
+        match self {
+            Region::Apac => &continent::APAC_CODES,
+            Region::EU => &continent::EU_CODES,
+            Region::NA => &continent::NA_CODE,
+            Region::SA => &continent::SA_CODE,
+        }
+    }
+
+    fn to_byte_set(regions: &[Region]) -> RegionContainer {
+        RegionContainer::Set(
+            regions
+                .iter()
+                .copied()
+                .flat_map(Region::to_bytes)
+                .copied()
+                .collect(),
+        )
     }
 }
 
@@ -196,8 +226,11 @@ impl Filters {
         !self.with_bots && !self.without_bots && self.include_unresponsive
     }
 
-    fn regions(&self) -> Option<HashSet<ContCode>> {
-        self.region.as_deref().map(Region::to_byte_set)
+    fn regions(&self, settings: &Settings) -> Option<RegionContainer> {
+        self.region
+            .as_deref()
+            .map(Region::to_byte_set)
+            .or_else(|| settings.regions())
     }
 }
 
@@ -205,6 +238,7 @@ impl Filters {
 async fn filter_server_list(
     repl: &mut ReplHandle,
     args: Filters,
+    settings: &Settings,
     spinner: Spinner,
 ) -> Result<FilterData, CmdErr> {
     let sources = args
@@ -225,9 +259,9 @@ async fn filter_server_list(
     ));
 
     if args.stats {
-        StatTrackStrategy::execute(repl, args, sources, spinner).await
+        StatTrackStrategy::execute(repl, args, settings, sources, spinner).await
     } else {
-        FastStrategy::execute(repl, args, sources, spinner).await
+        FastStrategy::execute(repl, args, settings, sources, spinner).await
     }
 }
 

src/commands/filter/ops.rs
@@ -1,14 +1,15 @@
 use super::{
-    Addressable, DEFAULT_INFO_RETRIES, GetInfoMetaData, LocationBatchRes, RETRY_TIME_SCALE,
-    Request, Server, Sourced, UnresponsiveCounter, strategies::FilterStrategy, try_get_info,
+    Addressable, GetInfoMetaData, LocationBatchRes, RETRY_TIME_SCALE, RegionContainer, Request,
+    Server, Sourced, UnresponsiveCounter, strategies::FilterStrategy, try_batched_location_lookup,
+    try_get_info,
 };
 use crate::{
     LOG_ONLY, ResponseErr, STATUS_OK,
-    commands::filter::try_batched_location_lookup,
+    commands::settings::Settings,
     make_slice_ascii_lowercase,
     models::{
         cli::{Filters, Source},
-        json_data::{ContCode, HostData},
+        json_data::HostData,
     },
     parse_hostname,
     utils::{
@@ -173,6 +174,7 @@ pub(crate) fn spawn_info_requests(
 pub(super) async fn join_info_requests<R>(
     mut requests: JoinSet<Result<Server, GetInfoMetaData>>,
     args: &Filters,
+    settings: &Settings,
     client: &Client,
     spinner: &Spinner,
     init: impl FnOnce(usize) -> R,
@@ -184,7 +186,7 @@ pub(super) async fn join_info_requests<R>(
     let mut responses = Vec::with_capacity(requests.len());
     let mut did_not_respond = UnresponsiveCounter::default();
     let mut sent_retires = false;
-    let max_attempts = args.retry_max.unwrap_or(DEFAULT_INFO_RETRIES);
+    let max_attempts = args.retry_max.unwrap_or(settings.server_retires);
     let server_info_endpoint = global_state::Endpoints::server_info_endpoint();
 
     while !requests.is_empty() {
@@ -291,7 +293,7 @@ pub(crate) fn process_region_requests((results, ips_requested): LocationBatchRes
 /// Does not retain the ordering of `servers`, internally appends valid new lookups to the end
 pub(super) async fn filter_via_region<S>(
     sourced_servers: &mut Vec<S>,
-    regions: &HashSet<ContCode>,
+    regions: &RegionContainer,
     client: &Client,
     spinner: &Spinner,
 ) -> bool

src/commands/filter/strategies.rs
@@ -1,12 +1,12 @@
 use super::{
     Addressable, DEFAULT_SOURCES, FilterData, GAME_ID, GetInfoMetaData, H2M_ID, HMW_ID, HostMeta,
-    Request, Server, Sourced, ops::*, try_get_info,
+    Request, Server, Sourced, ops::*, try_batched_location_lookup, try_get_info,
 };
 use crate::{
     client_with_timeout, command_err,
     commands::{
-        filter::try_batched_location_lookup,
         handler::{CmdErr, ReplHandle},
+        settings::Settings,
     },
     display::{indicator::Spinner, table::DisplaySourceStatsInner},
     elide,
@@ -46,6 +46,7 @@ pub(crate) trait FilterStrategy:
     async fn execute(
         repl: &mut ReplHandle,
         args: Filters,
+        settings: &Settings,
         sources: Option<HashSet<Source>>,
         spinner: Spinner,
     ) -> Result<FilterData, CmdErr>;
@@ -117,6 +118,7 @@ impl FilterStrategy for FastStrategy {
     async fn execute(
         _repl: &mut ReplHandle,
         mut args: Filters,
+        settings: &Settings,
         sources: Option<HashSet<Source>>,
         spinner: Spinner,
     ) -> Result<FilterData, CmdErr> {
@@ -125,7 +127,7 @@ impl FilterStrategy for FastStrategy {
         let mut sourced_servers = Self::new(sources, &client).await?;
         let mut cache_modified = false;
 
-        if let Some(regions) = args.regions() {
+        if let Some(regions) = args.regions(settings) {
             cache_modified =
                 filter_via_region(&mut sourced_servers.0, &regions, &client, &spinner).await
         }
@@ -137,6 +139,7 @@ impl FilterStrategy for FastStrategy {
             let (mut servers, mod_info) = join_info_requests(
                 requests,
                 &args,
+                settings,
                 &client,
                 &spinner,
                 Vec::with_capacity,
@@ -323,6 +326,7 @@ impl FilterStrategy for StatTrackStrategy {
     async fn execute(
         repl: &mut ReplHandle,
         mut args: Filters,
+        settings: &Settings,
         sources: Option<HashSet<Source>>,
         spinner: Spinner,
     ) -> Result<FilterData, CmdErr> {
@@ -346,6 +350,7 @@ impl FilterStrategy for StatTrackStrategy {
             join_info_requests(
                 requests.info,
                 &args,
+                settings,
                 &client,
                 &spinner,
                 HashMap::with_capacity,
@@ -361,8 +366,8 @@ impl FilterStrategy for StatTrackStrategy {
         let source_stats = stat_track.get_source_stats(&info_map);
         let mut servers = stat_track.collect_to_servers(&[HMW_ID], info_map);
 
-        if let Some(regions) = args.regions().as_ref() {
-            cache_modified |= filter_via_region(&mut servers, regions, &client, &spinner).await
+        if let Some(regions) = args.regions(settings) {
+            cache_modified |= filter_via_region(&mut servers, &regions, &client, &spinner).await
         }
 
         filter_via_get_info(&mut servers, &mut args);

src/commands/handler.rs
@@ -2,10 +2,11 @@ use crate::{
     CRATE_NAME, LOG_ONLY, MAIN_PROMPT, MOD_FILES_MODULE_NAME, ResponseErr, SAVED_HISTORY_CAP,
     commands::{
         filter::build_favorites,
-        launch_h2m::{
-            LaunchError, WinApiErr, game_open, hide_pseudo_console, init_listener,
-            launch_h2m_pseudo, toggle_close_state,
+        launch::{
+            LaunchError, WinApiErr, game_open, hide_pseudo_console, init_listener, spawn_pseudo,
+            toggle_close_state,
         },
+        settings::{Settings, SettingsRenderer},
     },
     exe_details,
     files::*,
@@ -14,13 +15,13 @@ use crate::{
         cli::{CacheCmd, Command, Filters},
         json_data::{CacheFile, CondManifest, HmwManifest, Version},
     },
-    open_dir, splash_screen,
+    open_dir, send_msg_over, splash_screen,
     utils::{
         caching::{build_cache, write_cache},
         display::{
             self, ConnectionHelp, DISP_NAME_HMW, DisplayLogs, HmwUpdateHelp, indicator::ProgressBar,
         },
-        global_state::{self, Cache, ThreadCopyState},
+        global_state::{self, AltScreen, Cache, ThreadCopyState},
     },
 };
 
@@ -62,7 +63,7 @@ use winptyrs::PTY;
 
 macro_rules! hmw_hash_err {
     ($($arg:tt)*) => {
-        error!("Could not get latest HMW version: {}", format_args!($($arg)*))
+        error!("Could not get latest HMW version - {}", format_args!($($arg)*))
     }
 }
 
@@ -183,6 +184,7 @@ impl GameDetails {
     pub fn get(
         cache: Option<&CacheFile>,
         exe_dir: &Path,
+        settings: &Settings,
     ) -> Result<(Self, bool), Cow<'static, str>> {
         let mod_verification = if cache.is_some_and(|cache| cache.hmw_manifest.verified) {
             ModFileStatus::UpToDate
@@ -199,7 +201,7 @@ impl GameDetails {
 
             #[cfg(debug_assertions)]
             true
-        };
+        } || !settings.launch_on_startup;
 
         #[cfg(not(debug_assertions))]
         {
@@ -209,7 +211,7 @@ impl GameDetails {
                     GameDetails::new(game_exe_path, version, hash, mod_verification)
                 }
                 Err(err) if no_launch => {
-                    splash_screen::push_message(Message::error(err));
+                    AltScreen::push_message(Message::error(err));
                     GameDetails::default(&exe_dir, mod_verification)
                 }
                 Err(err) => return Err(err),
@@ -461,6 +463,8 @@ pub struct CommandContext {
     pub game_state_change: Arc<Notify>,
     game: GameDetails,
     appdata: AppDetails,
+    pub settings: Settings,
+    pub(crate) settings_disp: SettingsRenderer,
     queued_con_task: Option<JoinHandle<()>>,
 }
 
@@ -503,6 +507,7 @@ impl Executor<Stdout> for CommandContext {
             Command::GameDir => self.try_open_game_dir(),
             Command::LocalEnv => self.try_open_local_dir(),
             Command::Version { verify_all } => self.print_version(verify_all).await,
+            Command::Settings { use_default } => self.settings(line_handle, use_default),
             Command::Quit => self.quit(),
         }
     }
@@ -512,8 +517,8 @@ pub struct StartupData {
     pub local_dir: Option<PathBuf>,
     pub game: GameDetails,
     pub appdata: AppDetails,
+    pub settings: Settings,
     pub cache_task: JoinHandle<StartupCacheContents>,
-    pub splash_task: JoinHandle<io::Result<()>>,
     pub launch_task: JoinHandle<Option<Result<PTY, LaunchError>>>,
     pub hmw_manifest_task: JoinHandle<Result<HmwManifest, ResponseErr>>,
 }
@@ -537,6 +542,7 @@ impl From<HookTag> for i32 {
 impl CommandContext {
     pub async fn from(
         mut startup_data: StartupData,
+        splash_task: JoinHandle<io::Result<()>>,
         term: Stdout,
     ) -> (ReplHandle, Self, Receiver<Message>) {
         let (launch_res, hmw_hash_res, cache_res) = tokio::join!(
@@ -548,7 +554,7 @@ impl CommandContext {
         macro_rules! hmw_launch_err {
             ($($arg:tt)*) => {{
                 error!("Could not launch H2M as child process: {}", format_args!($($arg)*));
-                splash_screen::push_message(Message::Str(ConnectionHelp.into()));
+                AltScreen::push_message(Message::Str(ConnectionHelp.into()));
                 None
             }}
         }
@@ -584,7 +590,7 @@ impl CommandContext {
                     .conditional_condense_manifest(latest_manifest);
 
                 if startup_data.game.prep_verification_state().is_err() {
-                    splash_screen::push_message(Message::str(
+                    AltScreen::push_message(Message::str(
                         startup_data.game.mod_verification.to_string(),
                     ));
                 } else if let (Some(hash_curr), Some(hash_latest)) = (
@@ -617,6 +623,8 @@ impl CommandContext {
             game_state_change: Arc::new(Notify::const_new()),
             appdata: startup_data.appdata,
             game: startup_data.game,
+            settings: startup_data.settings,
+            settings_disp: SettingsRenderer::new(),
             local_dir: startup_data.local_dir,
             can_close_console: true,
             queued_con_task: None,
@@ -626,7 +634,7 @@ impl CommandContext {
             ctx.listener_routine().unwrap_or_else(display::warning);
         }
 
-        splash_screen::leave(startup_data.splash_task).await;
+        splash_screen::leave(splash_task).await;
 
         (
             repl_builder(term)
@@ -643,6 +651,10 @@ impl CommandContext {
     #[inline]
     pub(crate) fn local_dir(&self) -> Option<&Path> {
         self.local_dir.as_deref()
+    }
+    #[inline]
+    pub(crate) fn get_exe_dir(&self) -> &Path {
+        self.game.get_exe_dir()
     }
     #[inline]
     pub(crate) fn msg_sender(&self) -> Sender<Message> {
@@ -732,7 +744,7 @@ impl CommandContext {
             return Ok(CommandHandle::Processed);
         }
 
-        match launch_h2m_pseudo(&self.game.path) {
+        match spawn_pseudo(&self.game.path) {
             Ok(conpty) => {
                 info!("Launching {}...", self.game_name());
                 self.game.update(exe_details(&self.game.path));
@@ -824,7 +836,7 @@ impl CommandContext {
             };
 
             for msg in messages {
-                msg_sender.send(msg).await.unwrap_or_else(|err| err.0.log());
+                send_msg_over(&msg_sender, msg).await;
             }
         });
         Ok(())
@@ -835,15 +847,7 @@ impl CommandContext {
         repl: &mut ReplHandle,
         args: Option<Filters>,
     ) -> io::Result<CommandHandle> {
-        let exe_dir = self.game.path.parent().expect("has parent");
-
-        let build_res = build_favorites(
-            repl,
-            exe_dir,
-            args.unwrap_or_default(),
-            self.game.version.unwrap_or(1.0),
-        )
-        .await;
+        let build_res = build_favorites(self, repl, args).await;
 
         if let Some(cache_modified) = CmdErr::transpose(build_res)? {
             global_state::UpdateCache::and_modify(|curr| curr || cache_modified);

src/commands/launch.rs
@@ -439,7 +439,7 @@ impl WinApiErr {
     }
 }
 
-pub fn launch_h2m_pseudo(game_path: &Path) -> Result<PTY, LaunchError> {
+pub fn spawn_pseudo(game_path: &Path) -> Result<PTY, LaunchError> {
     if let Some(game_name) = game_open()? {
         return Err(LaunchError::GameRunning(game_name));
     }

src/commands/reconnect.rs
@@ -3,7 +3,7 @@ use crate::{
     commands::{
         filter::{Request, Sourced, try_get_info},
         handler::{CmdErr, CommandContext, CommandHandle, CommandSender, Message, ReplHandle},
-        launch_h2m::HostName,
+        launch::HostName,
     },
     models::cli::HistoryArgs,
     parse_hostname, send_msg_over, try_fit_table,

commit 0023150294db622aa0cdd67be65f4b9a1a7b80db

Bug fix: startup can not create local app dir
Introduced in 62c7456, `check_app_dir_exists` no longer modifies a
reference but returns you the modified own Pathbuf. This ensures that
the correct directory gets created.

src/lib.rs
@@ -435,7 +435,7 @@ fn check_app_dir_exists(local: &Path) -> io::Result<PathBuf> {
     let curr_local_dir = local.join(CRATE_NAME);
 
     if !curr_local_dir.try_exists()? {
-        std::fs::create_dir(local)?;
+        std::fs::create_dir(&curr_local_dir)?;
     }
 
     if prev_local_dir