WardLordRuby/match_wire

commit fb91925b9b2b86298ff878542c0e37d8807e36c3

Update PTY interface
Cleaned up method signature for working with `PtyHandle`. New signature
cleans up the logic flow internally and avoids redundancy for callers by
also checking if the game is open in an "unattached" state.

src/commands/handler.rs
@@ -22,7 +22,7 @@ use crate::{
             self, ConnectionHelp, DISP_NAME_HMW, DisplayLogs, UPDATE_MSG_HMW,
             indicator::ProgressBar,
         },
-        main_thread_state::{self, Cache, ThreadCopyState},
+        main_thread_state::{self, Cache, PseudoConStatus, ThreadCopyState},
     },
 };
 
@@ -882,8 +882,8 @@ impl CommandContext {
             let mut attempt = 1;
             let messages = loop {
                 tokio::time::sleep(SLEEP * attempt).await;
-                match main_thread_state::PtyHandle::is_alive() {
-                    Ok(true) if attempt == 3 => {
+                match main_thread_state::PtyHandle::check_connection() {
+                    Ok(PseudoConStatus::Attached) if attempt == 3 => {
                         match hide_pseudo_console() {
                             Ok(true) => info!(name: LOG_ONLY, "Pseudo console window hidden"),
                             Ok(false) => {
@@ -894,8 +894,8 @@ impl CommandContext {
                         game_state_change.notify_one();
                         break vec![Message::info(format!("Connected to {game_name} console"))];
                     }
-                    Ok(true) => attempt += 1,
-                    Ok(false) => {
+                    Ok(PseudoConStatus::Attached) => attempt += 1,
+                    Ok(PseudoConStatus::Closed) | Ok(PseudoConStatus::Unattached) => {
                         break vec![
                             Message::error(format!(
                                 "Could not establish connection to {game_name}"
@@ -903,7 +903,7 @@ impl CommandContext {
                             Message::str(ConnectionHelp),
                         ];
                     }
-                    Err(err) => break vec![Message::error(err)],
+                    Err(err) => break vec![Message::error(err.to_string())],
                 }
             };
 

src/utils/main_thread_state.rs
@@ -24,6 +24,7 @@ use std::{
     borrow::Cow,
     cell::{Cell, LazyCell, OnceCell, RefCell},
     collections::HashMap,
+    ffi::OsString,
     fmt::Display,
     io,
     path::{Path, PathBuf},
@@ -513,29 +514,72 @@ impl AppHWND {
 }
 
 pub(crate) enum PtyAccessErr {
-    ConnectionErr(Cow<'static, str>),
+    PtyErr(OsString),
     UserErr(Cow<'static, str>),
+    NotAlive,
 }
 
-macro_rules! inner {
-    ($struct:expr) => {
-        match $struct {
-            PtyAccessErr::ConnectionErr(cow) => cow,
-            PtyAccessErr::UserErr(cow) => cow,
-        }
-    };
+impl PtyAccessErr {
+    pub(crate) fn is_connection_err(&self) -> bool {
+        matches!(self, PtyAccessErr::PtyErr(_) | PtyAccessErr::NotAlive)
+    }
+}
+
+impl From<OsString> for PtyAccessErr {
+    fn from(err: OsString) -> Self {
+        Self::PtyErr(err)
+    }
 }
 
 impl Display for PtyAccessErr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", inner!(self))
+        match self {
+            PtyAccessErr::PtyErr(err) => write!(f, "{}", err.to_string_lossy()),
+            PtyAccessErr::UserErr(err) => write!(f, "{err}"),
+            PtyAccessErr::NotAlive => write!(f, "Game console not currently connected"),
+        }
     }
 }
 
 impl From<PtyAccessErr> for Cow<'static, str> {
-    fn from(value: PtyAccessErr) -> Self {
-        inner!(value)
+    fn from(err: PtyAccessErr) -> Self {
+        match err {
+            PtyAccessErr::UserErr(cow) => cow,
+            err => err.to_string().into(),
+        }
     }
+}
+
+pub(crate) enum ConnectionErr {
+    PtyErr(OsString),
+    WinApiErr(WinApiErr),
+}
+
+impl Display for ConnectionErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::PtyErr(err) => write!(f, "{}", err.to_string_lossy()),
+            Self::WinApiErr(err) => write!(f, "{err}"),
+        }
+    }
+}
+
+impl From<OsString> for ConnectionErr {
+    fn from(err: OsString) -> Self {
+        Self::PtyErr(err)
+    }
+}
+
+impl From<WinApiErr> for ConnectionErr {
+    fn from(err: WinApiErr) -> Self {
+        Self::WinApiErr(err)
+    }
+}
+
+pub(crate) enum PseudoConStatus {
+    Attached,
+    Unattached,
+    Closed,
 }
 
 pub(crate) struct PtyHandle;
@@ -545,43 +589,45 @@ impl PtyHandle {
         PTY_HANDLE.set(pty)
     }
 
-    /// Prefer to use [`Self::check_connection`] where possible as it also will drop no longer alive `PTY`s.
-    pub(crate) fn is_alive() -> Result<bool, Cow<'static, str>> {
-        PTY_HANDLE.with_borrow(|handle| {
-            let Some(handle) = handle.as_ref() else {
-                return Err(Cow::Borrowed("No connection to game client is active"));
+    fn is_alive(handle: &mut Option<PTY>) -> Result<bool, OsString> {
+        let Some(game_console) = handle else {
+            return Ok(false);
+        };
+
+        let alive_res = game_console.is_alive();
+        if matches!(alive_res, Ok(false) | Err(_)) {
+            *handle = None
+        }
+        alive_res
+    }
+
+    pub(crate) fn check_connection() -> Result<PseudoConStatus, ConnectionErr> {
+        PTY_HANDLE.with_borrow_mut(|handle| {
+            if Self::is_alive(handle)? {
+                return Ok(PseudoConStatus::Attached);
+            }
+            let state = match game_open()? {
+                Some(_) => PseudoConStatus::Unattached,
+                None => PseudoConStatus::Closed,
             };
 
-            handle
-                .is_alive()
-                .map_err(|err| Cow::Owned(err.to_string_lossy().to_string()))
+            Ok(state)
         })
     }
 
-    pub(crate) fn check_connection() -> Result<(), Cow<'static, str>> {
-        if let Err(err) = {
-            match Self::is_alive() {
-                Ok(true) => Ok(()),
-                Ok(false) => Err(Cow::Borrowed("No connection to game client is active")),
-                Err(err) => Err(err),
-            }
-        } {
-            PTY_HANDLE.with_borrow_mut(|handle| *handle = None);
-            return Err(err);
-        };
-
-        Ok(())
-    }
-
-    pub(crate) fn try_if_alive<R>(
-        f: impl FnOnce(&PTY) -> Result<R, Cow<'static, str>>,
+    pub(crate) fn try_if_alive<R, E: Into<Cow<'static, str>>>(
+        f: impl FnOnce(&PTY) -> Result<R, E>,
     ) -> Result<R, PtyAccessErr> {
-        Self::check_connection().map_err(PtyAccessErr::ConnectionErr)?;
+        PTY_HANDLE.with_borrow_mut(|handle| {
+            if !Self::is_alive(handle)? {
+                return Err(PtyAccessErr::NotAlive);
+            }
 
-        PTY_HANDLE
-            .with_borrow(|handle| handle.as_ref().map(f))
-            .expect("early return ensures handle is some")
-            .map_err(PtyAccessErr::UserErr)
+            f(handle
+                .as_ref()
+                .expect("early return ensures handle is some"))
+            .map_err(|err| PtyAccessErr::UserErr(err.into()))
+        })
     }
 
     pub(crate) fn try_drop_pty(game_name: &str) {

src/commands/launch.rs
@@ -7,7 +7,7 @@ use crate::{
     parse_hostname, send_msg_over, strip_ansi_private_modes,
     utils::{
         display,
-        main_thread_state::{self, ThreadCopyState},
+        main_thread_state::{self, PseudoConStatus, ThreadCopyState},
     },
 };
 
@@ -268,12 +268,17 @@ async fn add_to_history(
 }
 
 pub fn init_listener(context: &mut CommandContext) -> Result<(), String> {
-    main_thread_state::PtyHandle::check_connection()?;
+    let game_name = context.game_name();
+
+    let PseudoConStatus::Attached =
+        main_thread_state::PtyHandle::check_connection().map_err(|err| err.to_string())?
+    else {
+        return Err(format!("No connection to {game_name} console"));
+    };
 
     let msg_sender = context.msg_sender();
     let game_state_change = context.game_state_change();
     let version = context.game_version().unwrap_or(1.0);
-    let game_name = context.game_name();
 
     tokio::spawn(async move {
         let mut buffer = OsString::new();
@@ -301,7 +306,7 @@ pub fn init_listener(context: &mut CommandContext) -> Result<(), String> {
                             buffer.push(os_string);
                             false
                         })
-                        .map_err(|err| err.to_string_lossy().to_string().into())
+                        .map_err(|err| err.to_string_lossy().to_string())
                 }) {
                     Ok(true) => break,
                     Ok(false) => tokio::task::yield_now().await,

src/commands/reconnect.rs
@@ -13,7 +13,7 @@ use crate::{
             ConnectionHelp, DisplayHistoryErr,
             table::{DisplayHistory, TABLE_PADDING},
         },
-        main_thread_state::{self, PtyAccessErr},
+        main_thread_state,
     },
 };
 
@@ -127,7 +127,7 @@ impl CommandContext {
             game_console.send_connect(ip_port)
         }) {
             error!("{err}");
-            if let PtyAccessErr::ConnectionErr(_) = err {
+            if err.is_connection_err() {
                 println!("{ConnectionHelp}");
             }
         }
@@ -173,9 +173,8 @@ impl CommandContext {
                     if let Err(err) = main_thread_state::PtyHandle::try_if_alive(|game_console| {
                         game_console.send_connect(addr)
                     }) {
-                        let connection_err = matches!(err, PtyAccessErr::ConnectionErr(_));
-                        send_msg_over(&msg_sender, Message::error(err)).await;
-                        if connection_err {
+                        send_msg_over(&msg_sender, Message::error(err.to_string())).await;
+                        if err.is_connection_err() {
                             send_msg_over(&msg_sender, Message::str(ConnectionHelp)).await;
                         }
                     } else if let Some(parsed) = hostname {

commit e142f3bfbb4c9e87b2f3d09f59941a358acc1ab4

Bug fix: close terminal window state
The state tracking for if the terminal window is safe to forcefully
close broke in fb91925 due to the change in function signature. This
change is now properly reflected.

src/commands/handler.rs
@@ -808,20 +808,30 @@ impl CommandContext {
             }
         };
 
-        if main_thread_state::PtyHandle::check_connection().is_ok() {
-            if game_open.is_some() {
+        let pty_active = match main_thread_state::PtyHandle::game_connected() {
+            Ok(status) => status,
+            Err(err) => {
+                error!(
+                    "{}, could not determine if it is okay to launch",
+                    err.to_string_lossy()
+                );
+                return Ok(CommandHandle::Processed);
+            }
+        };
+
+        if let Some(window_name) = game_open {
+            if pty_active {
                 println!(
                     "{GREEN}Connection to {} already active{RESET}",
                     self.game_name()
                 );
-                return Ok(CommandHandle::Processed);
+            } else {
+                println!("{RED}{window_name} is already running{RESET}\n{ConnectionHelp}");
             }
-            main_thread_state::PtyHandle::wait_for_exit(&self.game_name());
-        }
-
-        if let Some(window_name) = game_open {
-            println!("{RED}{window_name} is already running{RESET}\n{ConnectionHelp}");
             return Ok(CommandHandle::Processed);
+        } else if pty_active {
+            // Game window is not present, it must be in the process of closing
+            main_thread_state::PtyHandle::wait_for_exit(&self.game_name());
         }
 
         match spawn_pseudo(&self.game.path) {
@@ -833,6 +843,7 @@ impl CommandContext {
             }
             Err(err) => error!("{err}"),
         };
+
         Ok(CommandHandle::Processed)
     }
 
@@ -851,7 +862,9 @@ impl CommandContext {
         &mut self,
         line_handle: &mut ReplHandle,
     ) -> io::Result<Result<(), WinApiErr>> {
-        let game_open = main_thread_state::PtyHandle::check_connection().is_ok();
+        let game_open = main_thread_state::PtyHandle::game_connected()
+            .map_err(|err| display::log_error(err.to_string_lossy()))
+            .unwrap_or_default();
 
         line_handle.writer().queue(SetTitle(CRATE_NAME))?;
 

src/utils/main_thread_state.rs
@@ -603,6 +603,12 @@ impl PtyHandle {
         alive_res
     }
 
+    /// Only checks if the PTY console is alive, if you also need more information about the games current open
+    /// state use [`Self::check_connection`]
+    pub(crate) fn game_connected() -> Result<bool, OsString> {
+        PTY_HANDLE.with_borrow_mut(Self::is_alive)
+    }
+
     pub(crate) fn check_connection() -> Result<PseudoConStatus, ConnectionErr> {
         PTY_HANDLE.with_borrow_mut(|handle| {
             if Self::is_alive(handle)? {